11df11b901ebeda03e4e25c08ff14e4cbf90418d
JOAO LUCAS GOMES NAPOLES <1-22-11755@aluno.faminas.edu.br>
2024-03-21 22:06:32 -0300

    models e data

---
+++ data/mock-data.js
@@ -0,0 +1,29 @@
+import Company from '../models/Company'
+import Vacancy from '../models/Vacancy'
+
+export const VACANCIES = [
+    new Vacancy(
+        1,
+        'Helpdesk nivel 1',
+        1,
+        'Lorem ipsum',
+        'Lorem ipsum',
+        'https://picsum.photos/200'
+    ),
+    new Vacancy(
+        2,
+        'Programador Web Front-End',
+        1,
+        'Lorem ipsum',
+        'Lorem ipsum',
+        'https://picsum.photos/200'
+    ),
+    new Vacancy(
+        1,
+        'Helpdesk nivel 1',
+        1,
+        'Lorem ipsum',
+        'Lorem ipsum',
+        'https://picsum.photos/200'
+    )
+]
---
+++ models/Vacancy.js
@@ -0,0 +1,19 @@
+class Vacancy {
+    constructor(
+        id,
+        title,
+        id_company,
+        requirements,
+        differential,
+        banner
+    ){
+        this.id = id,
+        this.title = title,
+        this.id_company = id_company,
+        this.requirements = requirements,
+        this.differential = differential,
+        this.banner = banner
+    }
+}
+
+export default Vacancy
